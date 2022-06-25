ea01c709ee695f05ed35f9402f82728837bb15ee
trunovandrey21 <97969331+trunovandrey21@users.noreply.github.com>
2022-06-25 19:48:53 +0200

    Add files via upload

---
+++ tooltip/script/script.js
@@ -0,0 +1,24 @@
+const createTooltip = (element, text) => {
+    const tooltip = document.createElement('div');
+    tooltip.classList.add('tooltip');
+
+    tooltip.innerText = text;
+
+    document.body.append(tooltip);
+
+    const anchorElemCoords = element.getBoundingClientRect();
+
+    const tooltipTop = anchorElemCoords.top - 10 - tooltip.clientHeight;
+
+    const tooltipLeft = 
+    anchorElemCoords.left +
+    anchorElemCoords.width / 2 -
+    tooltip.clientWidth / 2;
+
+    tooltip.style.top = `${tooltipTop}px`;
+    tooltip.style.left = `${tooltipLeft}px`;
+};
+
+const button = document.grtElementById('example');
+
+createTooltip(button, 'tooltip for button');
